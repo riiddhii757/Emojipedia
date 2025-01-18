7f50331fd1693521600aa9885e2d039208f07d6b
riiddhii757 <riddhhi757@gmail.com>
2025-01-17 21:54:18 -0500

    Initial commit with React mapping props

---
+++ src/components/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+//id: 1,
+//emoji: "💪",
+//name: "Tense Biceps",
+//meaning:
+ //"“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+
+function createEntry(emojiTerm){
+return (
+  <Entry
+  key={emojiTerm.id}
+  emoji={emojiTerm.emoji}
+  name={emojiTerm.name}
+  description={emojiTerm.meaning}
+  />
+);
+}
+
+
+function App() {
+  return (     
+   <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+       {emojipedia.map(createEntry)}
+        
+      </dl>
+    </div> 
+  );
+}
+
+export default App;
